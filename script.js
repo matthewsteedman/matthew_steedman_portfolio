537efb0397f6cbc5a9a2468db172d4dc5de4c58b
matthewsteedman <71071832+matthewsteedman@users.noreply.github.com>
2021-01-08 16:27:01 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -1,20 +1,51 @@
-$(document).ready(function() {
-    console.log("click")
-    $(window).scroll(function() {
+$(document).ready(function () {
+  console.log("click");
+  $(window).scroll(function () {
+    if (this.scrollY > 20) {
+      $(".navbar").addClass("sticky");
+    } else {
+      $(".navbar").removeClass("sticky");
+    }
+    if (this.scroll > 500) {
+      $(".scroll-up-btn").addClass("show");
+    } else {
+      $(".scroll-up-btn").removeClass("show");
+    }
+  });
 
-            if (this.scrollY > 20) {
-                $('.navbar').addClass('sticky');
+  // slide-up script
 
-            } else {
-                $('.navbar').removeClass('sticky');
-            }
+  $(".scroll-up-btn").click(function () {
+    $("html").animate({ scrollTop: 0 });
+  });
 
-        })
-        // toggle menu/navbar script
+  // toggle menu/navbar script
 
-    $('.menu-btn').click(function() {
-        console.log("menu opened")
-        $('.navbar .menu').toggleClass("active")
-        $('.menu-btn i').toggleClass("active")
-    })
+  $(".menu-btn").click(function () {
+    console.log("menu opened");
+    $(".navbar .menu").toggleClass("active");
+    $(".menu-btn i").toggleClass("active");
+  });
+
+  // owl carousel
+  $(".carousel").owlCarousel({
+    margin: 20,
+    loop: true,
+    autoplayTimeOut: 2000,
+    autoplayHoverPause: true,
+    responsive: {
+      0: {
+        items: 1,
+        nav: false,
+      },
+      600: {
+        items: 2,
+        nav: false,
+      },
+      1000: {
+        items: 3,
+        nav: false,
+      },
+    },
+  });
 });
