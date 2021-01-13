8d238808200416111e3b077c379dc538fb7424da
matthewsteedman <71071832+matthewsteedman@users.noreply.github.com>
2021-01-13 11:06:56 +0200

    Create mail.js

---
+++ mail.js
@@ -0,0 +1,8 @@
+name = document.getElementbyId("user_name");
+email = document.getElementbyId("user_email");
+subject = document.getElementbyId("user_subject");
+message = document.getElementbyId("user_message");
+
+function clear(){
+  name = ''
+}
